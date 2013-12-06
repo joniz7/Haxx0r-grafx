54545c45d3f2a65ddb9e1c9f435e616e3ad2d1e2
Jonathan Orro <jonathan.orro@gmail.com>
2013-12-06 14:28:11 +0100

    Added eval and started readExpr. Also added the Parsing library

--- Expr.hs
+++ Expr.hs
@@ -1,7 +1,9 @@
-data Expr = Num Double | Var String | Sin Expr | Cos Expr | Add Expr Expr | Mul Expr Expr
-instance Show Expr where
-    show = showExpr
+import Parsing
 
+data Expr = Num Double | Var String | Sin Expr | Cos Expr | Add Expr Expr | Mul Expr Expr
+--instance Show Expr where
+  -- show = showExpr
+{-
 showExpr :: Expr -> String
 showExpr (Num i) = show i
 showExpr (Var s) = s
@@ -9,9 +11,28 @@ showExpr (Sin e) = "sin " ++ paranthesis e
 showExpr (Cos e) = "sin " ++ paranthesis e
 showExpr (Add e1 e2) = showExpr e1 ++ " + " ++ showExpr e2
 showExpr (Mul e1 e2) = needParanthesis e1 ++ "*" ++ needParanthesis e2
-   where paranthesis exp = "(" ++ showExpr exp ++ ")"
-         paranthesis (Num i) = showExpr (Num i)
+   where paranthesis (Num i) = showExpr (Num i)
          paranthesis (Var s) = showExpr (Var s)
-		 needParanthesis (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
-		 
-		 
+         paranthesis exp = "(" ++ showExpr exp ++ ")"
+         needParanthesis (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
+-}
+
+-- (5+6)*8*9 = 792
+ex1 = Mul (Add (Num 5) (Num 6)) (Mul (Num 8) (Num 9))
+
+-- sin(7+2*1)
+ex2 = Sin (Add (Num 7) (Mul (Num 2) (Num 1)))
+
+-- cos(x*4+6*2) + sin(x)
+ex3 = Add (Cos (Add (Mul (Var "x") (Num 4)) (Mul (Num 6) (Num 2)))) (Sin (Var "x"))
+
+eval :: Expr -> Double -> Double
+eval (Num i) _     = i
+eval (Var s) i     = i
+eval (Add e1 e2) x = eval e1 x + eval e2 x
+eval (Mul e1 e2) x = eval e1 x * eval e2 x
+eval (Sin e)     x = sin (eval e x)
+eval (Cos e)     x = cos (eval e x)
+
+readExpr :: String -> Maybe Int
+readExpr = undefined
