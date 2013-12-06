4c6d51d4ce7c5d8046adf85d6c51b38f0bdfd579
Jonathan Orro <jonathan.orro@gmail.com>
2013-12-06 14:38:46 +0100

    Did all the stuff.

--- Expr.hs
+++ Expr.hs
@@ -1,21 +1,22 @@
 import Parsing
 
 data Expr = Num Double | Var String | Sin Expr | Cos Expr | Add Expr Expr | Mul Expr Expr
---instance Show Expr where
-  -- show = showExpr
-{-
+instance Show Expr where
+   show = showExpr
+
 showExpr :: Expr -> String
-showExpr (Num i) = show i
-showExpr (Var s) = s
-showExpr (Sin e) = "sin " ++ paranthesis e
-showExpr (Cos e) = "sin " ++ paranthesis e
-showExpr (Add e1 e2) = showExpr e1 ++ " + " ++ showExpr e2
-showExpr (Mul e1 e2) = needParanthesis e1 ++ "*" ++ needParanthesis e2
-   where paranthesis (Num i) = showExpr (Num i)
-         paranthesis (Var s) = showExpr (Var s)
-         paranthesis exp = "(" ++ showExpr exp ++ ")"
-         needParanthesis (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
--}
+showExpr e = case e of
+    (Num i) -> show i
+    (Var s) -> s
+    (Sin e) -> "sin " ++ paranthesis e
+    (Cos e) -> "cos " ++ paranthesis e
+    (Add e1 e2) -> showExpr e1 ++ " + " ++ showExpr e2
+    (Mul e1 e2) -> needParanthesis e1 ++ "*" ++ needParanthesis e2
+
+    where paranthesis (Num i)         = showExpr (Num i)
+          paranthesis (Var s)         = showExpr (Var s)
+          paranthesis exp             = "(" ++ showExpr exp ++ ")"
+          needParanthesis (Add e1 e2) = "(" ++ showExpr (Add e1 e2) ++ ")"
 
 -- (5+6)*8*9 = 792
 ex1 = Mul (Add (Num 5) (Num 6)) (Mul (Num 8) (Num 9))
